fa75e5a26b08e5b8e3147f38cd3c14fa4f572190
Alex Krat <>
2018-09-25 18:20:38 +0300

    - control commit

--- app/src/main/java/dev/challenge/boxio/presenters/MainPresenter.java
+++ app/src/main/java/dev/challenge/boxio/presenters/MainPresenter.java
@@ -2,13 +2,11 @@ package dev.challenge.boxio.presenters;
 
 import javax.inject.Inject;
 
-import dev.challenge.boxio.model.BoxEntity;
-import dev.challenge.boxio.model.ColorEntity;
-import dev.challenge.boxio.model.UserEntity;
+import dev.challenge.boxio.model.room.dao.BoxDao;
+import dev.challenge.boxio.model.room.dao.ColorDao;
 import dev.challenge.boxio.model.room.dao.UserDao;
 import dev.challenge.boxio.util.SharedPreferencesManager;
 import dev.challenge.boxio.view.activities.MainActivity;
-import io.reactivex.Observable;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.CompositeDisposable;
 import io.reactivex.schedulers.Schedulers;
@@ -16,38 +14,50 @@ import io.reactivex.schedulers.Schedulers;
 public class MainPresenter extends AbstractPresenter<MainActivity> {
 
     private UserDao userDao;
+    private BoxDao boxDao;
+    private ColorDao colorDao;
     private SharedPreferencesManager sharedPreferencesManager;
     private CompositeDisposable compositeDisposable;
 
     @Inject
-    public MainPresenter(UserDao userDao, SharedPreferencesManager sharedPreferencesManager) {
+    public MainPresenter(UserDao userDao, BoxDao boxDao, ColorDao colorDao, SharedPreferencesManager sharedPreferencesManager) {
         this.userDao = userDao;
+        this.boxDao = boxDao;
+        this.colorDao = colorDao;
         this.sharedPreferencesManager = sharedPreferencesManager;
         compositeDisposable = new CompositeDisposable();
     }
 
     public void doSmth() {
-        UserEntity userEntity =
-                new UserEntity("Jumanji",
-                        "jumanji@gmail.com",
-                        new BoxEntity(BoxEntity.BoxSize.Medium.name(),
-                                new ColorEntity("Black", "000000")));
+//        UserEntity userEntity =
+//                new UserEntity("Jumanji",
+//                        "jumanji@gmail.com",
+//                        new BoxEntity(BoxEntity.BoxSize.Medium.name(),
+//                                new ColorEntity("Black", "000000")));
+//
+//        new CompositeDisposable().add(Observable.fromCallable(() -> {
+//            userDao.insertUser(userEntity);
+//            return userEntity;
+//        }).subscribeOn(Schedulers.io())
+//                .observeOn(AndroidSchedulers.mainThread())
+//                .subscribe(user -> System.out.println("Inserted user: " + user)));
 
-        new CompositeDisposable().add(Observable.fromCallable(() -> {
-            userDao.insertUser(userEntity);
-            return userEntity;
-        }).subscribeOn(Schedulers.io())
+        compositeDisposable.add(userDao.getUsers().subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(user -> System.out.println("Inserted user: " + user)));
+                .subscribe(users -> System.out.println("\n\nUsers from DB: " + users)));
 
-        new CompositeDisposable().add(userDao.getUsers().subscribeOn(Schedulers.io())
+        compositeDisposable.add(boxDao.getBoxes().subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(users -> System.out.println("Users from DB: " + users)));
+                .subscribe(users -> System.out.println("\n\nBoxes from DB: " + users)));
+
+        compositeDisposable.add(colorDao.getColors().subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(users -> System.out.println("\n\nColors from DB: " + users)));
     }
 
     @Override
     void cancel() {
-
+        compositeDisposable.clear();
     }
 
     @Override
